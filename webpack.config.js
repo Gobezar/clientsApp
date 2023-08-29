fb0e3a88f0521c197b0394969faa43f80f90b6cb
Gobezar <vadimkiselevv@yandex.ru>
2023-08-29 14:13:29 +0400

    fix5

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  // entry: "./src/index.tsx",
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
