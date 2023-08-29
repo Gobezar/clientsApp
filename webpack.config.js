899047d9e4ce50120a6260e2369178a6c1d35534
Gobezar <vadimkiselevv@yandex.ru>
2023-08-29 13:32:24 +0400

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,6 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
-      // {
-      //   test: /\.(png|jpg|jpeg|gif)$/i,
-      //   type: "asset/resource",
-      // },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         generator: {
