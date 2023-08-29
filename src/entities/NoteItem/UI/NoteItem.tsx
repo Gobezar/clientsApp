59a49818772f7a1423ba2a34b989506ba8c0da97
Gobezar <vadimkiselevv@yandex.ru>
2023-08-29 19:01:32 +0400

    fix popup

--- src/entities/NoteItem/UI/NoteItem.tsx
+++ src/entities/NoteItem/UI/NoteItem.tsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Button from "../../../shared/Button/UI/Button";
 import { Notes } from "../../../widgets/ClientsList/consts/clients";
 import "./NoteItem.scss";
 
 const NoteItem = ({ note }: { note: Notes }) => {
+  const [openPopup, setOpenPopup] = useState(false);
+  const noteRef = useRef<HTMLDivElement>(null);
+
+  const popupItems = [
+    { id: 1, name: "Изменить" },
+    { id: 2, name: "Удалить" },
+  ];
+
+  useEffect(() => {
+    const clickOutSide = (event: MouseEvent | React.MouseEvent) => {
+      const path = (event as any).composedPath().includes(noteRef.current);
+      if (!path) {
+        setOpenPopup(false);
+      }
+    };
+    document.body.addEventListener("click", clickOutSide);
+    return () => {
+      document.body.removeEventListener("click", clickOutSide);
+    };
+  }, []);
+
   return (
     <div className="noteItemContainer">
       <div className="noteItemText">
         <span>{note.date}</span> {note.text}{" "}
         {note.image && <img src={note.image} alt="note" />}
       </div>
-      <div>
-        <Button theme="additionallyTransparent" />
+      <div ref={noteRef}>
+        <Button
+          theme="additionallyTransparent"
+          onClick={() => setOpenPopup(!openPopup)}
+        />
+        {openPopup && (
+          <div className="popupWrapper">
+            <ul>
+              {popupItems.map((obj) => (
+                <li className="popup" key={obj.id}>
+                  {obj.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
