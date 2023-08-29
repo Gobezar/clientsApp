7daf3c101d23ac0154207e9c593f06900c88e59f
Gobezar <vadimkiselevv@yandex.ru>
2023-08-29 18:52:44 +0400

    fix popup

--- src/widgets/CurrentClientSection/UI/CurrentClientSection.tsx
+++ src/widgets/CurrentClientSection/UI/CurrentClientSection.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useAppSelector } from "../../../store/reduxHooks";
 import Button from "../../../shared/Button/UI/Button";
 import "./CurrentClientSection.scss";
 
 const CurrentClientSection = () => {
   const { currentClient } = useAppSelector((state) => state.CurrentClientSlice);
+  const [openPopup, setOpenPopup] = useState(false);
+  const Ref = useRef<HTMLDivElement>(null);
 
-  //   const fullName = `${currentClient[0].surname} ${currentClient[0].name} ${currentClient[0].secondName}`;
+  const popupItems = [
+    { id: 1, name: "Изменить" },
+    { id: 2, name: "Удалить" },
+  ];
+
+  useEffect(() => {
+    const clickOutSide = (event: MouseEvent | React.MouseEvent) => {
+      const path = (event as any).composedPath().includes(Ref.current);
+      if (!path) {
+        setOpenPopup(false);
+      }
+    };
+    document.body.addEventListener("click", clickOutSide);
+    return () => {
+      document.body.removeEventListener("click", clickOutSide);
+    };
+  }, []);
 
   return (
     <div className="currentClientContainer">
@@ -28,8 +46,22 @@ const CurrentClientSection = () => {
               </div>
             </div>
 
-            <div className="buttonWrapper">
-              <Button theme="additionallyTransparent" />
+            <div className="buttonWrapper" ref={Ref}>
+              <Button
+                theme="additionallyTransparent"
+                onClick={() => setOpenPopup(!openPopup)}
+              />
+              {openPopup && (
+                <div className="popupWrapper">
+                  <ul>
+                    {popupItems.map((obj) => (
+                      <li className="popup" key={obj.id}>
+                        {obj.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
         </>
